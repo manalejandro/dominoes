8e2b3fdb3fce10b0da0a7fc531fd0920ef4b4e50
ale <ale@manalejandro.com>
2025-11-11 23:40:16 +0100

    fix accessibility
    Signed-off-by: ale <ale@manalejandro.com>

--- app/page.tsx
+++ app/page.tsx
@@ -352,7 +352,7 @@ export default function Home() {
                     ))}
                   </div>
                 </div>
-              )}
+              ))}
           </aside>
         </div>
       </main>
